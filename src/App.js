5153872d4d46b1f253ee930f2207ecfed9e77769
MagdaMaggie <magdalena-sophie.dunz@gmx.de>
2024-08-08 11:36:25 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Three from './components/Three';
+import './css/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Three />
+
+      <section className="one">
+        <div className="container">
+          <div className="hero">
+            <h2>Wartungsanleitung einer High-Z S-1000 T CNC Fräsmaschine</h2>
+            <p>
+                Im Folgenden werden die einzelnen Schritte der Wartung beschrieben. <br />
+                Die Abkürzung CNC steht für „Computerized Numerical Control" <br />
+                (CNC-Fräsen: Grundlagen und Beispiele von Horst-Christian Heinkle)
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="two">
+        <div className="container">
+          <div className="hero">
+            <h2>Vorbereitung</h2>
+            <p>
+              Als erstes ist es notwendig den Strom anzuschalten<br />
+              und die Brücke nach ganz links zu fahren.<br />
+              Danach mit einem Pinsel alle Holzspähne entfernen<br />
+              und Handschuhe, ein Putztuch sowie Fett bereitlegen.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="three">
+        <div className="container">
+          <div className="hero">
+            <h2>1. Schritt</h2>
+            <p>
+                Handschuhe anziehen und die Brücke in die Mitte fahren.<br />
+                Rechts und links von der Spindel der x-Achse etwas Fett geben<br />
+                und danach die Brücke nach links und rechts fahren.<br />
+                Dadurch wird das alte Fett nach außen getrieben<br /> 
+                und kann anschließend mit einem Lappen abgetragen werden.<br />
+                Widerholen bis das ganze alte Fett abgetragen wurde.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      
+      <section className="four">
+          <div className="container">
+              <div className="hero">
+                  <h2>2. Schritt</h2>
+                  <p>
+                      Stirnseitig an der Maschine das Abdeckblech der Z-Achse entfernen.<br />
+                      Dazu die drei Schrauben mit einem Imbusschlüssel ziehen.<br />
+                      Die Abdeckung nach oben herausnehmen.<br />
+                      Wartungsschritt 1 an der Z-Achse anwenden, <br />
+                      wie zuvor an der X-Achse.<br />                 
+                  </p>
+              </div>
+          </div>
+      </section>
+
+      
+      <section className="five">
+          <div className="container">
+              <div className="hero">
+                  <h2>3. Schritt</h2>
+                  <p>
+                      Nun auf die andere Siete der Z-Achse wechseln.<br />
+                      Dort die Antriebsspindel der Z-Achse vor und hinter<br />
+                      der Umlaufmutter einfetten, mit dem selben Prinzip<br />
+                      wie zuvor schon bei den anderen beiden Achsen.
+                  </p>
+              </div>
+          </div>
+      </section>
+
+      
+      <section className="six">
+          <div className="container">
+              <div className="hero">
+                  <h2>4. Schritt</h2>
+                  <p>
+                      Als nächstes die Motorlager der Schrittmotore abschmieren.<br />
+                      Dafür die Z-Achse nach ganz unten fahren,<br />
+                      dann ist der entsprechende Schmiernippel bereits zu sehen.<br />
+                      Mit einer Fettpresse diese Stelle einfetten.
+                  </p>
+              </div>
+          </div>
+      </section>
+
+      
+      <section className="seven">
+          <div className="container">
+              <div className="hero">
+                  <h2>5. Schritt</h2>
+                  <p>
+                      Alle Spindeln mit neuem Fett einfetten.<br />
+                      Zum Schluss noch die Abdeckung der Z-Achse montieren.<br />
+                      Dazu diese einfelden und verschrauben.<br />
+                      Die Maschinen in all ihren Achsen verschieben, <br />
+                      damit sich das Fett schön verteilt.
+                  </p>
+              </div>
+          </div>
+      </section>
+
+      
+      <section className="eight">
+          <div className="container">
+              <div className="hero">
+                  <h1>Ende</h1>
+              </div>
+          </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/Three.js
@@ -0,0 +1,215 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+// import { HDRCubeTextureLoader } from "three/examples/jsm/loaders/HDRCubeTextureLoader.js";
+import { PMREMGenerator } from "three/src/extras/PMREMGenerator.js";
+import { gsap } from "gsap";
+
+function MyThree() {
+  const canvasRef = useRef(null);
+  const sceneRef = useRef(null);
+  const rendererRef = useRef(null);
+  const cameraRef = useRef(null);
+  const modelRef = useRef(null);
+  const mixerRef = useRef(null);
+
+  // Three js logic after the component mounts
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const scene = new THREE.Scene();
+    sceneRef.current = scene;
+
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const camera = new THREE.PerspectiveCamera(
+      35,
+      sizes.width / sizes.height,
+      0.1,
+      1000
+    );
+    camera.position.z = 5;
+    scene.add(camera);
+    cameraRef.current = camera;
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true,
+      alpha: true,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.6;
+    rendererRef.current = renderer;
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 2, 0);
+    scene.add(directionalLight);
+
+    const loadModel = () => {
+      const fraesenUrl = new URL("../assets/7.glb", import.meta.url).href;
+      const gLTFLoader = new GLTFLoader();
+      gLTFLoader.load(
+        fraesenUrl,
+        (gltf) => {
+          const model = gltf.scene;
+          model.position.set(2.5, -0.5, 0);
+          model.rotation.set(Math.PI * 0.1, Math.PI * 1.2, 0);
+          model.scale.set(1, 1, 1);
+
+          model.traverse((child) => {
+            if (child.isMesh && child.material.map) {
+              // child.material.map.encoding = THREE.sRGBEncoding;
+            }
+          });
+
+          scene.add(model);
+          modelRef.current = model;
+
+          const mixer = new THREE.AnimationMixer(model);
+          mixerRef.current = mixer;
+
+          gltf.animations.forEach((clip) => {
+            const action = mixer.clipAction(clip);
+            action.play();
+          });
+        },
+        undefined,
+        (error) => console.error("An error happened while loading the model", error)
+      );
+    };
+
+    const loadHDR = () => {
+      // const hdrUrls = [
+      //   "px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg",
+      // ];
+      const pmremGenerator = new PMREMGenerator(renderer);
+      pmremGenerator.compileCubemapShader();
+
+      new RGBELoader().load('../assets/environment.hdr', (texture)=> {
+        texture.mapping=THREE.EquirectangularReflectionMapping;
+        scene.background=texture;
+        scene.environment=texture;
+        })
+
+      // Wirft ein Fehler, weil die Dateien nicht gefunden werden
+      // new HDRCubeTextureLoader()
+      //   .setPath("../assets/CubeHdr/")
+      //   .load(
+      //     hdrUrls,
+      //     (hdrCubeMap) => {
+      //       const hdrCubeRenderTarget = pmremGenerator.fromCubemap(hdrCubeMap);
+      //       hdrCubeMap.magFilter = THREE.LinearFilter;
+      //       hdrCubeMap.needsUpdate = true;
+
+      //       scene.background = hdrCubeRenderTarget.texture;
+      //       scene.environment = hdrCubeRenderTarget.texture;
+      //       console.log("pmremGenerator", hdrCubeMap);
+      //     },
+      //     undefined,
+      //     (error) => console.error("An error happened while loading the HDR textures", error)
+      //   );
+      
+    };
+
+    const animate = () => {
+      const clock = new THREE.Clock();
+      let lastElapsedTime = 0;
+
+      const tick = () => {
+        const elapsedTime = clock.getElapsedTime();
+        const deltaTime = elapsedTime - lastElapsedTime;
+        lastElapsedTime = elapsedTime;
+
+        if (mixerRef.current) {
+          mixerRef.current.update(deltaTime);
+        }
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(tick);
+      };
+
+      tick();
+    };
+
+    const handleResize = () => {
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    };
+
+    const handleScroll = () => {
+      const transformModel = [
+        { rotationZ: 0, positionX: 2.5 },
+        { rotationZ: 0, positionX: -1.3 },
+        { rotationZ: 0, positionX: 2.5 },
+        { rotationZ: 0, positionX: -1.3 },
+        { rotationZ: 0, positionX: 2.5 },
+        { rotationZ: 0, positionX: -1.3 },
+        { rotationZ: 0, positionX: 2.5 },
+        { rotationZ: 0, positionX: 0 },
+      ];
+
+      let currentSection = 0;
+
+      const scrollHandler = () => {
+        const newSection = Math.round(window.scrollY / sizes.height);
+
+        if (newSection !== currentSection) {
+          currentSection = newSection;
+
+          if (modelRef.current && transformModel[currentSection]) {
+            gsap.to(modelRef.current.rotation, {
+              duration: 1.25,
+              ease: "power2.inOut",
+              z: transformModel[currentSection].rotationZ,
+            });
+            gsap.to(modelRef.current.position, {
+              duration: 1.25,
+              ease: "power2.inOut",
+              x: transformModel[currentSection].positionX,
+            });
+          }
+        }
+      };
+
+      window.addEventListener("scroll", scrollHandler);
+      return () => window.removeEventListener("scroll", scrollHandler);
+    };
+
+    loadModel();
+    loadHDR();
+    animate();
+    handleScroll();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      // Clean up Three.js resources
+      if (rendererRef.current) {
+        rendererRef.current.dispose();
+      }
+      if (sceneRef.current) {
+        sceneRef.current.clear();
+      }
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} className="webgl" />;
+}
+
+export default MyThree;
